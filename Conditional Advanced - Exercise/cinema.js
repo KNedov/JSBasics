6dfcb156c41a2ca0080744b80834882079845ee4
Krasimir <k.medov.90@gmail.com>
2024-06-05 11:42:36 -0700

    Update cinema.js
    update

--- Conditional Advanced - Exercise/cinema.js
+++ Conditional Advanced - Exercise/cinema.js
@@ -1,13 +1,17 @@
 function cinema(input) {
-    let projection = input[0];
-    let lines = Number(input[1]);
-    let columbs = Number(input[2]);
-    switch (projection) {
-        case "Premiere": console.log(`${(lines * columbs * 12).toFixed(2)} leva`); break;
-        case "Normal": console.log(`${(lines * columbs * 7.50).toFixed(2)} leva`); break;
-        case "Discount": console.log(`${(lines * columbs * 5.00).toFixed(2)} leva`); break;
-    }
-
-} cinema(["Normal",
-    "21",
-    "13"])
+  let projection = input[0];
+  let lines = Number(input[1]);
+  let columbs = Number(input[2]);
+  switch (projection) {
+    case "Premiere":
+      console.log(`${(lines * columbs * 12).toFixed(2)} leva`);
+      break;
+    case "Normal":
+      console.log(`${(lines * columbs * 7.5).toFixed(2)} leva`);
+      break;
+    case "Discount":
+      console.log(`${(lines * columbs * 5.0).toFixed(2)} leva`);
+      break;
+  }
+}
+cinema(["Normal", "21", "13"]);
